99401d3faffe13714f8a0bb224032f53bd806c29
zhouchunsen <2570408456@qq.com>
2021-06-06 22:50:01 +0800

    Revert "第一次提交"
    This reverts commit 6922280b

--- text/src/main/java/com/text/demo/Demo.java
+++ text/src/main/java/com/text/demo/Demo.java
@@ -3,8 +3,6 @@ package com.text.demo;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("serer");
-        System.out.println("serer");
-        System.out.println("serer");
-        System.out.println("serer");
+        System.out.println("text");
     }
 }
